1becb83e13e328de99db070cdbff1a8906751ae5
Tim Hirzel <tim@zapatacomputing.com>
2022-05-06 12:02:18 -0400

    updating example workflow file

---
+++ workflow_defs.py
@@ -0,0 +1,18 @@
+################################################################################
+# © Copyright 2021-2022 Zapata Computing Inc.
+################################################################################
+
+import orquestra.sdk.v2 as sdk
+
+THIS_IMPORT = sdk.GitImport(
+    repo_url="git@github.com:zapatacomputing/tutorial-0-welcome.git",
+    git_ref="main",
+)
+
+@sdk.task(source_import=THIS_IMPORT)
+def hello():
+    return "Hello Orquestra!"
+
+@sdk.workflow
+def hello_workflow():
+    return [hello()]
